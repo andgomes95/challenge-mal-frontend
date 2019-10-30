83e959b0427ff3474f90c940dce4a93fcafea67e
andgomes95 <andgomes95@gmail.com>
2019-10-30 13:45:42 -0300

    test fake json

--- src/app/shared/services/desafios.service.ts
+++ src/app/shared/services/desafios.service.ts
@@ -4,63 +4,13 @@ import { Observable,throwError,of } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
 import { Desafio } from '../models/desafio.model';
 
-const httpOptions = {
-    headers: new HttpHeaders({'Content-Type' : 'application/json'})
-  };
-  const apiUrl = "api/desafios";
+export class Cursos2Service extends CrudService<Curso> {
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DesafiosService {
-
-  constructor(
-    private http: HttpClient
-  ) {} 
-  baseUrl: string = 'https://challenge-mal-frontend.stackblitz.io/api/';
-
- getDesafios(){
-   return this.http.get<Desafio[]>(this.baseUrl);
- }
-
-  getProducts (): Observable<Desafio[]> {
-  return this.http.get<Desafio[]>(apiUrl)
-    .pipe(
-      tap(heroes => console.log('fetched challenges')),
-      catchError(this.handleError('getChallenges', []))
-    );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-  private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
-
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
-  protected extractData(res: Response | any) {
-  
-    try {
-      return <any>res.json();
-    }
-    catch (e) {
-      return {};
-    }
+  constructor(protected http: HttpClient) {
+    super(http, `${environment.API}cursos`);
   }
 
+  loadByID(id) {
+    return null;
+  }
 }
