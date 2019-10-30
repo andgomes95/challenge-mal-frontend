03c755898e6278bf176f2e00d442b583ca55714f
andgomes95 <andgomes95@gmail.com>
2019-10-30 13:31:32 -0300

    fix registration button

--- src/app/modules/desafios/desafios.component.ts
+++ src/app/modules/desafios/desafios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Desafio } from '../../shared/models/desafio.model';
 
 @Component({
   selector: 'app-desafios',
--- src/app/shared/services/desafios.service.ts
+++ src/app/shared/services/desafios.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable,throwError } from 'rxjs';
+import { Observable,throwError,of } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
 import { Desafio } from '../models/desafio.model';
 
 const httpOptions = {
@@ -22,6 +23,13 @@ export class DesafiosService {
    return this.http.get<Desafio[]>(this.baseUrl);
  }
 
+  getProducts (): Observable<Desafio[]> {
+  return this.http.get<Desafio[]>(apiUrl)
+    .pipe(
+      tap(heroes => console.log('fetched challenges')),
+      catchError(this.handleError('getChallenges', []))
+    );
+}
 
 
 
@@ -35,10 +43,16 @@ export class DesafiosService {
 
 
 
-  private handleError(error: any) {
-    console.error(error);                                       //Created a function to handle and log errors, in case
-    return throwError(error);
-  }
+  private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
   protected extractData(res: Response | any) {
   
     try {
